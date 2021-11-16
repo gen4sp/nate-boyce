2d2b025fcc80549c3d877ace1612dc063fd93e4c
gen4 <gen4mail@gmail.com>
2021-11-16 15:15:32 +0300

    upd

--- engine/Pavel/index.js
+++ engine/Pavel/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-
+import SHADERS from './shaders'
 export default function (canvas) {
   //   const canvas = document.getElementsByTagName('canvas')[0]
   resizeCanvas()
@@ -15,21 +15,11 @@ export default function (canvas) {
     CURL: 30,
     SPLAT_RADIUS: 0.25,
     SPLAT_FORCE: 6000,
-    SHADING: false,
     COLORFUL: false,
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
     BACK_COLOR: { r: 0, g: 0, b: 0 },
-    TRANSPARENT: true,
-    BLOOM: false,
-    BLOOM_ITERATIONS: 8,
-    BLOOM_RESOLUTION: 256,
-    BLOOM_INTENSITY: 0.8,
-    BLOOM_THRESHOLD: 0.6,
-    BLOOM_SOFT_KNEE: 0.7,
-    SUNRAYS: false,
-    SUNRAYS_RESOLUTION: 196,
-    SUNRAYS_WEIGHT: 1.0
+    TRANSPARENT: false
   }
 
   function PointerPrototype() {
@@ -252,367 +242,44 @@ export default function (canvas) {
     return keywordsString + source
   }
 
-  const baseVertexShader = compileShader(
-    gl.VERTEX_SHADER,
-    `
-        precision highp float;
-    
-        attribute vec2 aPosition;
-        varying vec2 vUv;
-        varying vec2 vL;
-        varying vec2 vR;
-        varying vec2 vT;
-        varying vec2 vB;
-        uniform vec2 texelSize;
-    
-        void main () {
-            vUv = aPosition * 0.5 + 0.5;
-            vL = vUv - vec2(texelSize.x, 0.0);
-            vR = vUv + vec2(texelSize.x, 0.0);
-            vT = vUv + vec2(0.0, texelSize.y);
-            vB = vUv - vec2(0.0, texelSize.y);
-            gl_Position = vec4(aPosition, 0.0, 1.0);
-        }
-    `
-  )
+  const baseVertexShader = compileShader(gl.VERTEX_SHADER, SHADERS.baseVert)
 
-  const copyShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        uniform sampler2D uTexture;
-    
-        void main () {
-            gl_FragColor = texture2D(uTexture, vUv);
-        }
-    `
-  )
+  const copyShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.copyFrag)
 
-  const clearShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        uniform sampler2D uTexture;
-        uniform float value;
-    
-        void main () {
-            gl_FragColor = value * texture2D(uTexture, vUv);
-        }
-    `
-  )
+  const clearShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.clearFrag)
 
-  const colorShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-    
-        uniform vec4 color;
-    
-        void main () {
-            gl_FragColor = color;
-        }
-    `
-  )
+  const colorShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.colorFrag)
 
   const checkerboardShader = compileShader(
     gl.FRAGMENT_SHADER,
-    `
-        precision highp float;
-        precision highp sampler2D;
-    
-        varying vec2 vUv;
-        uniform sampler2D uTexture;
-        uniform float aspectRatio;
-    
-        #define SCALE 25.0
-    
-        void main () {
-            vec2 uv = floor(vUv * SCALE * vec2(aspectRatio, 1.0));
-            float v = mod(uv.x + uv.y, 2.0);
-            v = v * 0.1 + 0.8;
-            gl_FragColor = vec4(vec3(v), 1.0);
-        }
-    `
+    SHADERS.checkboardFrag
   )
 
-  const displayShaderSource = `
-        precision highp float;
-        precision highp sampler2D;
-    
-        varying vec2 vUv;
-        varying vec2 vL;
-        varying vec2 vR;
-        varying vec2 vT;
-        varying vec2 vB;
-        uniform sampler2D uTexture;
-        uniform sampler2D uBloom;
-        uniform sampler2D uSunrays;
-        uniform sampler2D uDithering;
-        uniform vec2 ditherScale;
-        uniform vec2 texelSize;
-    
-        vec3 linearToGamma (vec3 color) {
-            color = max(color, vec3(0));
-            return max(1.055 * pow(color, vec3(0.416666667)) - 0.055, vec3(0));
-        }
-    
-        void main () {
-            vec3 c = texture2D(uTexture, vUv).rgb;
-    
-        #ifdef SHADING
-            vec3 lc = texture2D(uTexture, vL).rgb;
-            vec3 rc = texture2D(uTexture, vR).rgb;
-            vec3 tc = texture2D(uTexture, vT).rgb;
-            vec3 bc = texture2D(uTexture, vB).rgb;
-    
-            float dx = length(rc) - length(lc);
-            float dy = length(tc) - length(bc);
-    
-            vec3 n = normalize(vec3(dx, dy, length(texelSize)));
-            vec3 l = vec3(0.0, 0.0, 1.0);
-    
-            float diffuse = clamp(dot(n, l) + 0.7, 0.7, 1.0);
-            c *= diffuse;
-        #endif
-    
-        #ifdef BLOOM
-            vec3 bloom = texture2D(uBloom, vUv).rgb;
-        #endif
-    
-        #ifdef SUNRAYS
-            float sunrays = texture2D(uSunrays, vUv).r;
-            c *= sunrays;
-        #ifdef BLOOM
-            bloom *= sunrays;
-        #endif
-        #endif
-    
-        #ifdef BLOOM
-            float noise = texture2D(uDithering, vUv * ditherScale).r;
-            noise = noise * 2.0 - 1.0;
-            bloom += noise / 255.0;
-            bloom = linearToGamma(bloom);
-            c += bloom;
-        #endif
-    
-            float a = max(c.r, max(c.g, c.b));
-            gl_FragColor = vec4(c, a);
-        }
-    `
-
-  const splatShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision highp float;
-        precision highp sampler2D;
-    
-        varying vec2 vUv;
-        uniform sampler2D uTarget;
-        uniform float aspectRatio;
-        uniform vec3 color;
-        uniform vec2 point;
-        uniform float radius;
-    
-        void main () {
-            vec2 p = vUv - point.xy;
-            p.x *= aspectRatio;
-            vec3 splat = exp(-dot(p, p) / radius) * color;
-            vec3 base = texture2D(uTarget, vUv).xyz;
-            gl_FragColor = vec4(base + splat, 1.0);
-        }
-    `
-  )
+  const splatShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.splatFrag)
 
   const advectionShader = compileShader(
     gl.FRAGMENT_SHADER,
-    `
-        precision highp float;
-        precision highp sampler2D;
-    
-        varying vec2 vUv;
-        uniform sampler2D uVelocity;
-        uniform sampler2D uSource;
-        uniform vec2 texelSize;
-        uniform vec2 dyeTexelSize;
-        uniform float dt;
-        uniform float dissipation;
-    
-        vec4 bilerp (sampler2D sam, vec2 uv, vec2 tsize) {
-            vec2 st = uv / tsize - 0.5;
-    
-            vec2 iuv = floor(st);
-            vec2 fuv = fract(st);
-    
-            vec4 a = texture2D(sam, (iuv + vec2(0.5, 0.5)) * tsize);
-            vec4 b = texture2D(sam, (iuv + vec2(1.5, 0.5)) * tsize);
-            vec4 c = texture2D(sam, (iuv + vec2(0.5, 1.5)) * tsize);
-            vec4 d = texture2D(sam, (iuv + vec2(1.5, 1.5)) * tsize);
-    
-            return mix(mix(a, b, fuv.x), mix(c, d, fuv.x), fuv.y);
-        }
-    
-        void main () {
-        #ifdef MANUAL_FILTERING
-            vec2 coord = vUv - dt * bilerp(uVelocity, vUv, texelSize).xy * texelSize;
-            vec4 result = bilerp(uSource, coord, dyeTexelSize);
-        #else
-            vec2 coord = vUv - dt * texture2D(uVelocity, vUv).xy * texelSize;
-            vec4 result = texture2D(uSource, coord);
-        #endif
-            float decay = 1.0 + dissipation * dt;
-            gl_FragColor = result / decay;
-        }`,
+    SHADERS.advectionFrag,
     ext.supportLinearFiltering ? null : ['MANUAL_FILTERING']
   )
 
   const divergenceShader = compileShader(
     gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        varying highp vec2 vL;
-        varying highp vec2 vR;
-        varying highp vec2 vT;
-        varying highp vec2 vB;
-        uniform sampler2D uVelocity;
-    
-        void main () {
-            float L = texture2D(uVelocity, vL).x;
-            float R = texture2D(uVelocity, vR).x;
-            float T = texture2D(uVelocity, vT).y;
-            float B = texture2D(uVelocity, vB).y;
-    
-            vec2 C = texture2D(uVelocity, vUv).xy;
-            if (vL.x < 0.0) { L = -C.x; }
-            if (vR.x > 1.0) { R = -C.x; }
-            if (vT.y > 1.0) { T = -C.y; }
-            if (vB.y < 0.0) { B = -C.y; }
-    
-            float div = 0.5 * (R - L + T - B);
-            gl_FragColor = vec4(div, 0.0, 0.0, 1.0);
-        }
-    `
+    SHADERS.divergenceFrag
   )
 
-  const curlShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        varying highp vec2 vL;
-        varying highp vec2 vR;
-        varying highp vec2 vT;
-        varying highp vec2 vB;
-        uniform sampler2D uVelocity;
-    
-        void main () {
-            float L = texture2D(uVelocity, vL).y;
-            float R = texture2D(uVelocity, vR).y;
-            float T = texture2D(uVelocity, vT).x;
-            float B = texture2D(uVelocity, vB).x;
-            float vorticity = R - L - T + B;
-            gl_FragColor = vec4(0.5 * vorticity, 0.0, 0.0, 1.0);
-        }
-    `
-  )
+  const curlShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.curlFrag)
 
   const vorticityShader = compileShader(
     gl.FRAGMENT_SHADER,
-    `
-        precision highp float;
-        precision highp sampler2D;
-    
-        varying vec2 vUv;
-        varying vec2 vL;
-        varying vec2 vR;
-        varying vec2 vT;
-        varying vec2 vB;
-        uniform sampler2D uVelocity;
-        uniform sampler2D uCurl;
-        uniform float curl;
-        uniform float dt;
-    
-        void main () {
-            float L = texture2D(uCurl, vL).x;
-            float R = texture2D(uCurl, vR).x;
-            float T = texture2D(uCurl, vT).x;
-            float B = texture2D(uCurl, vB).x;
-            float C = texture2D(uCurl, vUv).x;
-    
-            vec2 force = 0.5 * vec2(abs(T) - abs(B), abs(R) - abs(L));
-            force /= length(force) + 0.0001;
-            force *= curl * C;
-            force.y *= -1.0;
-    
-            vec2 velocity = texture2D(uVelocity, vUv).xy;
-            velocity += force * dt;
-            velocity = min(max(velocity, -1000.0), 1000.0);
-            gl_FragColor = vec4(velocity, 0.0, 1.0);
-        }
-    `
+    SHADERS.vorticityFrag
   )
 
-  const pressureShader = compileShader(
-    gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        varying highp vec2 vL;
-        varying highp vec2 vR;
-        varying highp vec2 vT;
-        varying highp vec2 vB;
-        uniform sampler2D uPressure;
-        uniform sampler2D uDivergence;
-    
-        void main () {
-            float L = texture2D(uPressure, vL).x;
-            float R = texture2D(uPressure, vR).x;
-            float T = texture2D(uPressure, vT).x;
-            float B = texture2D(uPressure, vB).x;
-            float C = texture2D(uPressure, vUv).x;
-            float divergence = texture2D(uDivergence, vUv).x;
-            float pressure = (L + R + B + T - divergence) * 0.25;
-            gl_FragColor = vec4(pressure, 0.0, 0.0, 1.0);
-        }
-    `
-  )
+  const pressureShader = compileShader(gl.FRAGMENT_SHADER, SHADERS.pressureFrag)
 
   const gradientSubtractShader = compileShader(
     gl.FRAGMENT_SHADER,
-    `
-        precision mediump float;
-        precision mediump sampler2D;
-    
-        varying highp vec2 vUv;
-        varying highp vec2 vL;
-        varying highp vec2 vR;
-        varying highp vec2 vT;
-        varying highp vec2 vB;
-        uniform sampler2D uPressure;
-        uniform sampler2D uVelocity;
-    
-        void main () {
-            float L = texture2D(uPressure, vL).x;
-            float R = texture2D(uPressure, vR).x;
-            float T = texture2D(uPressure, vT).x;
-            float B = texture2D(uPressure, vB).x;
-            vec2 velocity = texture2D(uVelocity, vUv).xy;
-            velocity.xy -= vec2(R - L, T - B);
-            gl_FragColor = vec4(velocity, 0.0, 1.0);
-        }
-    `
+    SHADERS.gradientSubstract
   )
 
   const blit = (() => {
@@ -653,11 +320,11 @@ export default function (canvas) {
   let divergence
   let curl
   let pressure
-  let bloom
-  let sunrays
+  // let bloom
+  // let sunrays
   // let sunraysTemp
 
-  const ditheringTexture = createTextureAsync('LDR_LLL1_0.png')
+  // const ditheringTexture = createTextureAsync('LDR_LLL1_0.png')
 
   // const blurProgram = new Program(blurVertexShader, blurShader)
   const copyProgram = new Program(baseVertexShader, copyShader)
@@ -683,7 +350,7 @@ export default function (canvas) {
     gradientSubtractShader
   )
 
-  const displayMaterial = new Material(baseVertexShader, displayShaderSource)
+  const displayMaterial = new Material(baseVertexShader, SHADERS.displayFrag)
 
   function initFramebuffers() {
     const simRes = getResolution(config.SIM_RESOLUTION)
@@ -859,53 +526,8 @@ export default function (canvas) {
     return target
   }
 
-  function createTextureAsync(url) {
-    const texture = gl.createTexture()
-    gl.bindTexture(gl.TEXTURE_2D, texture)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.RGB,
-      1,
-      1,
-      0,
-      gl.RGB,
-      gl.UNSIGNED_BYTE,
-      new Uint8Array([255, 255, 255])
-    )
-
-    const obj = {
-      texture,
-      width: 1,
-      height: 1,
-      attach(id) {
-        gl.activeTexture(gl.TEXTURE0 + id)
-        gl.bindTexture(gl.TEXTURE_2D, texture)
-        return id
-      }
-    }
-
-    const image = new Image()
-    image.onload = () => {
-      obj.width = image.width
-      obj.height = image.height
-      gl.bindTexture(gl.TEXTURE_2D, texture)
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image)
-    }
-    image.src = url
-
-    return obj
-  }
-
   function updateKeywords() {
     const displayKeywords = []
-    if (config.SHADING) displayKeywords.push('SHADING')
-    if (config.BLOOM) displayKeywords.push('BLOOM')
-    if (config.SUNRAYS) displayKeywords.push('SUNRAYS')
     displayMaterial.setKeywords(displayKeywords)
   }
 
@@ -1114,28 +736,19 @@ export default function (canvas) {
   }
 
   function drawDisplay(target) {
-    const width = target == null ? gl.drawingBufferWidth : target.width
-    const height = target == null ? gl.drawingBufferHeight : target.height
-
     displayMaterial.bind()
-    if (config.SHADING)
-      gl.uniform2f(
-        displayMaterial.uniforms.texelSize,
-        1.0 / width,
-        1.0 / height
-      )
     gl.uniform1i(displayMaterial.uniforms.uTexture, dye.read.attach(0))
-    if (config.BLOOM) {
-      gl.uniform1i(displayMaterial.uniforms.uBloom, bloom.attach(1))
-      gl.uniform1i(
-        displayMaterial.uniforms.uDithering,
-        ditheringTexture.attach(2)
-      )
-      const scale = getTextureScale(ditheringTexture, width, height)
-      gl.uniform2f(displayMaterial.uniforms.ditherScale, scale.x, scale.y)
-    }
-    if (config.SUNRAYS)
-      gl.uniform1i(displayMaterial.uniforms.uSunrays, sunrays.attach(3))
+    // if (config.BLOOM) {
+    //   gl.uniform1i(displayMaterial.uniforms.uBloom, bloom.attach(1))
+    //   gl.uniform1i(
+    //     displayMaterial.uniforms.uDithering,
+    //     ditheringTexture.attach(2)
+    //   )
+    //   const scale = getTextureScale(ditheringTexture, width, height)
+    //   gl.uniform2f(displayMaterial.uniforms.ditherScale, scale.x, scale.y)
+    // }
+    // if (config.SUNRAYS)
+    //   gl.uniform1i(displayMaterial.uniforms.uSunrays, sunrays.attach(3))
     blit(target)
   }
 
@@ -1386,13 +999,6 @@ export default function (canvas) {
     if (gl.drawingBufferWidth > gl.drawingBufferHeight)
       return { width: max, height: min }
     else return { width: min, height: max }
-  }
-
-  function getTextureScale(texture, width, height) {
-    return {
-      x: width / texture.width,
-      y: height / texture.height
-    }
   }
 
   function scaleByPixelRatio(input) {
---
+++ engine/Pavel/shaders/index.js
@@ -0,0 +1,59 @@
+import advectionFrag from './advection.frag'
+import baseVert from './base.vert'
+import blurFrag from './blur.frag'
+import blurVert from './blur.vert'
+import checkboardFrag from './checkboard.frag'
+import clearFrag from './clear.frag'
+import colorFrag from './color.frag'
+import copyFrag from './copy.frag'
+import curlFrag from './curl.frag'
+import displayFrag from './display.frag'
+import divergenceFrag from './divergence.frag'
+import gradientSubstract from './gradientSubstract.frag'
+import pressureFrag from './pressure.frag'
+import splatFrag from './splat.frag'
+import vorticityFrag from './vorticity.frag'
+
+const SHADERS_SRCS = {
+  advectionFrag,
+  baseVert,
+  blurVert,
+  blurFrag,
+  checkboardFrag,
+  clearFrag,
+  colorFrag,
+  copyFrag,
+  curlFrag,
+  displayFrag,
+  divergenceFrag,
+  gradientSubstract,
+  pressureFrag,
+  splatFrag,
+  vorticityFrag
+}
+// const SHADERS_UNFORMS = {
+//   advectionFrag:{
+//     uVelocity:null,
+//     uSource:null,
+//     texelSize: null,
+//     dyeTexelSize: null;
+//     uniform float dt;
+//     uniform float dissipation;
+//   },
+//   baseVert,
+//   blurVert,
+//   blurFrag,
+//   checkboardFrag,
+//   clearFrag,
+//   colorFrag,
+//   copyFrag,
+//   curlFrag,
+//   displayFrag,
+//   divergenceFrag,
+//   gradientSubstract,
+//   pressureFrag,
+//   splatFrag,
+//   vorticityFrag
+// }
+
+export default SHADERS_SRCS
